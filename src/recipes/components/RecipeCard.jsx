23c50c24e8545f0f3f1277f9e993c238a4fb5307
Luis Angel Ponce Alvarez <poncealvarezluisangel4@gmail.com>
2023-06-12 22:17:30 -0600

    cambio importar las img

--- src/recipes/components/RecipeCard.jsx
+++ src/recipes/components/RecipeCard.jsx
@@ -3,7 +3,7 @@ import { P, P1, CardContainer, IMG, Div } from "../helpers/recipeCard.Styled";
 
 const RecipeCard = ({ id, name, type, url, time }) => {
   // OBJETO PARA CAPTURAR LA HACER CLICK, QUE LUEGO PASARÉ COMO PROP ALA FUNCION QUE IMPORTO DESDE EL CONTEXT
-  const imgUrl = `/assets/images/${name}.jpg`;
+  const imgUrl = `../assets/images/${name}.jpg`;
 
   return (
     <>
